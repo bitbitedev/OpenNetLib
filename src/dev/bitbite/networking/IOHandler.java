353c2c8a9e691224a492b8bf39661967b197f584
Ben <benmihm@n3tc0.de>
2021-02-04 20:06:54 +0100

    Added Listener to IOHandler
    - Added listeners to IOHandler
    - Added IOHandlerListener class

--- src/dev/bitbite/networking/IOHandler.java
+++ src/dev/bitbite/networking/IOHandler.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.function.Consumer;
 
 /**
@@ -23,6 +24,27 @@ public class IOHandler {
 	private PrintWriter writer;
 	private BufferedReader reader;
 	private Thread inputListener;
+	private ArrayList<IOHandlerListener> listeners;
+	
+	/**
+	 * The different event-types, which occur in the IOHandler, listeners can listen on
+	 * 
+	 * @see IOHandlerListener
+	 * @version 0.0.1-alpha
+	 */
+	enum EventType {
+		DATA_READ_START,
+		DATA_READ_END,
+		DATA_READ_FAILED,
+		
+		CLOSE_START,
+		CLOSE_END,
+		CLOSE_FAILED,
+		
+		WRITE,
+		WRITE_END,
+		WRITE_FAILED
+	}
 	
 	/**
 	 * Initializes the IOHandler with the given Streams and read-Callback method.<br>
@@ -42,16 +64,19 @@ public class IOHandler {
 		}
 		this.writer = new PrintWriter(outputStream);
 		this.reader = new BufferedReader(new InputStreamReader(inputStream));
+		this.listeners = new ArrayList<IOHandlerListener>();
 		this.inputListener = new Thread(()->{
+			notifyListeners(EventType.DATA_READ_START);
 			while(!Thread.interrupted()) {
 				try {
 					String message = reader.readLine();
 					onRead.accept(message);
 				} catch (IOException e) {
-					//TODO add Handler
+					notifyListeners(EventType.DATA_READ_FAILED);
 					e.printStackTrace();
 				}
 			}
+			notifyListeners(EventType.DATA_READ_END);
 		});
 		this.inputListener.setName("IO InputListener");
 		this.inputListener.start();
@@ -63,8 +88,14 @@ public class IOHandler {
 	 * @version 0.0.2-alpha
 	 */
 	public void close() throws IOException {
-		this.reader.close();
-		this.writer.close();
+		notifyListeners(EventType.CLOSE_START);
+		try {
+			this.reader.close();
+			this.writer.close();
+		} catch(Exception e) {
+			notifyListeners(EventType.CLOSE_FAILED);
+		}
+		notifyListeners(EventType.CLOSE_END);
 	}
 	
 	/**
@@ -74,8 +105,72 @@ public class IOHandler {
 	 * @see java.io.PrintWriter
 	 */
 	public void write(String data) {
-		this.writer.write(data+"\n");
-		this.writer.flush();
+		notifyListeners(EventType.WRITE);
+		try {
+			this.writer.write(data+"\n");
+			this.writer.flush();
+		} catch(Exception e) {
+			notifyListeners(EventType.WRITE_FAILED);
+		}
+		notifyListeners(EventType.WRITE_END);
+	}
+	
+	/**
+	 * Calls the respective function of each listener depending on the event type.<br>
+	 * Optionally Propagates additional info such as exceptions.
+	 * 
+	 * @param type of event that occured
+	 * @param args optional additional data
+	 * 
+	 * @throws IllegalArgumentException if additional arguments are supplied
+	 * whose types do not match the expected types of the listeners eventfunction
+	 * 
+	 * @see IOHandlerListener
+	 * 
+	 * @version 0.0.1-alpha
+	 */
+	private void notifyListeners(EventType type, Object... args) {
+		switch(type) {
+			case DATA_READ_START:
+				listeners.forEach(l -> l.onDataReadStart());
+				break;
+			case DATA_READ_END:
+				listeners.forEach(l -> l.onDataReadEnd());
+				break;
+			case DATA_READ_FAILED:
+				if(!(args[0] instanceof Exception)) {
+					throw new IllegalArgumentException("Expected object of type Exception, but got "+args[0].getClass().getSimpleName());
+				}
+				listeners.forEach(l -> l.onDataReadFailed((Exception)args[0]));
+				break;
+			case CLOSE_START:
+				listeners.forEach(l -> l.onCloseStart());
+				break;
+			case CLOSE_END:
+				listeners.forEach(l -> l.onCloseEnd());
+				break;
+			case CLOSE_FAILED:
+				if(!(args[0] instanceof Exception)) {
+					throw new IllegalArgumentException("Expected object of type Exception, but got "+args[0].getClass().getSimpleName());
+				}
+				listeners.forEach(l -> l.onCloseFailed((Exception)args[0]));
+				break;
+			case WRITE:
+				if(!(args[0] instanceof String)) {
+					throw new IllegalArgumentException("Expected object of type String, but got "+args[0].getClass().getSimpleName());
+				}
+				listeners.forEach(l -> l.onWrite((String)args[0]));
+				break;
+			case WRITE_END:
+				listeners.forEach(l -> l.onWriteEnd());
+				break;
+			case WRITE_FAILED:
+				if(!(args[0] instanceof Exception)) {
+					throw new IllegalArgumentException("Expected object of type Exception, but got "+args[0].getClass().getSimpleName());
+				}
+				listeners.forEach(l -> l.onWriteFailed((Exception)args[0]));
+				break;
+		}
 	}
 	
 }
---
+++ src/dev/bitbite/networking/IOHandlerListener.java
@@ -0,0 +1,63 @@
+package dev.bitbite.networking;
+
+/**
+ * This class contains all functions which, if registered at the IOHandler object, will be called
+ * when certain events are happening.
+ *
+ * @see IOHandler
+ *
+ * @version 0.0.1-alpha
+ */
+public abstract class IOHandlerListener {
+
+	/**
+	 * Will be called before the process of reading data from the inputStream
+	 * is being started
+	 */
+	public void onDataReadStart() {}
+
+	/**
+	 * Will be called once the process of reading data from the inputStream ends
+	 */
+	public void onDataReadEnd() {}
+	
+	/**
+	 * Will be called if an error occures while trying to read data from the inputStream
+	 * @param exception which was thrown
+	 */
+	public void onDataReadFailed(Exception exception) {}
+
+	/**
+	 * Will be called before the closing of the streams is started
+	 */
+	public void onCloseStart() {}
+
+	/**
+	 * Will be called once the streams have been closed
+	 */
+	public void onCloseEnd() {}
+	
+	/**
+	 * Will be called if an error occures while trying to close the streams
+	 * @param exception which was thrown during the process
+	 */
+	public void onCloseFailed(Exception exception) {}
+
+	/**
+	 * Will be called before data is sent over the outputStream
+	 * @param data which will be sent
+	 */
+	public void onWrite(String data) {}
+
+	/**
+	 * Will be called after the writing to the outputStream has been finished
+	 */
+	public void onWriteEnd() {}
+	
+	/**
+	 * Will be called if the writing the data to the outputStream fails
+	 * @param exception which was thrown while trying to send the data
+	 */
+	public void onWriteFailed(Exception exception) {}
+
+}
